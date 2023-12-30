d6081fea37723e6886efc9cd3f6c7696a1ab11c2
Rahul Mondal <rahulmondar@gmail.com>
2023-12-30 17:27:54 +0530

    feat: added docker-compose file and prettier config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  // Lint then format TypeScript and JavaScript files
-  "**/*.(ts|tsx|js)": (filenames) => [
-    `pnpm run lint`,
-    `pnpm dlx prettier --write ${filenames.join(" ")}`,
-  ],
+    // Type check TypeScript files
+    "**/*.(ts|tsx)": () => "pnpm exec tsc --noEmit",
 
-  // Format MarkDown and JSON
-  "**/*.(md|json)": (filenames) =>
-    `pnpm dlx prettier --write ${filenames.join(" ")}`,
+    // Lint then format TypeScript and JavaScript files
+    "**/*.(ts|tsx|js)": (filenames) => [
+        `pnpm run lint`,
+        `pnpm dlx prettier --write ${filenames.join(" ")}`,
+    ],
+
+    // Format MarkDown and JSON
+    "**/*.(md|json)": (filenames) =>
+        `pnpm dlx prettier --write ${filenames.join(" ")}`,
 };
